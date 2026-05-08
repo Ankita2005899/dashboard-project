25708a7841a63382116e8109dffb5570be6825b8
Ankita Bandal <ankitabandal45@gmail.com>
2026-05-08 15:22:12 +0530

    Fixed Procfile

--- app.py
+++ app.py
@@ -3277,3 +3277,11 @@ def serve_video(filename):
         filename,
         mimetype='video/mp4'
     )
+    
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
