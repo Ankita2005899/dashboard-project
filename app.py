637b5ce1ee834e4b0815671aaab99bdb3159c097
Ankita Bandal <ankitabandal45@gmail.com>
2026-05-21 21:06:59 +0530

    fix mail port 587

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ app.secret_key = "secret123"
 
 # ================= EMAIL CONFIGURATION =================
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'ankitabandal45@gmail.com'
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'eplorronxoysxury')
 app.config['MAIL_DEFAULT_SENDER'] = 'ankitabandal45@gmail.com'
